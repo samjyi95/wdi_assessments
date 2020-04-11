da0865c7f1dd6b6b87ec5029fc1f484fa615687c
samjyi95 <samjyi95@gmail.com>
2020-04-11 16:57:18 -0700

    retake completed

---
+++ assessment1Retake/script.js
@@ -0,0 +1,33 @@
+var f_temp = document.getElementById('input')
+var convert = document.getElementById('search')
+document.getElementById('input').defaultValue='32';
+document.getElementById('result').defaultValue='0';
+//let newNum = dosument.getElementById('result')
+
+//(f - 32) * 5 / 9
+function farToCel() {
+    let temp = document.getElementById('input').value
+    let newTemp = (parseInt(temp) - 32) * 5 / 9
+    console.log(newTemp)
+    result.innerHTML = newTemp
+    if (newTemp > 30) {
+        result.style.color = 'red'
+    }
+    else {
+        result.style.color = 'blue'
+    }
+}convert.addEventListener('click', farToCel)
+
+
+// function decor() {
+//     //let newNum = dosument.getElementById('result').value
+//     result.innerHTML = newTemp
+//     console.log('okay')
+//     if (newTemp.value < 30) {
+//         result.style.color = 'red'
+//     }
+//     else {
+//         result.style.color = 'blue'
+//     }
+//     result.innerHTML = newTemp
+// }
